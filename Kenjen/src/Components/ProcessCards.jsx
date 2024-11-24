0983ef706d00ba53da0052340f779887084fabc0
EbadullahSiddiqi <official.ebadullahsiddiqi@gmail.com>
2024-11-25 03:46:21 +0500

    Some adjustments made in ProcessCards

--- Kenjen/src/Components/ProcessCards.jsx
+++ Kenjen/src/Components/ProcessCards.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 export default function ProcessCards({ number, title, item1, item2, item3 }) {
   return (
-    <div className="proces-card rounded-xl border w-full max-w-full sm:max-w-sm md:max-w-md lg:max-w-lg bg-black/50">
+    <div className="proces-card rounded-xl border w-full max-w-full sm:max-w-sm md:max-w-md lg:max-w-lg bg-black/30">
       <div className="flex flex-col p-4 sm:p-6 md:p-8 gap-3 sm:gap-4 md:gap-6">
         {/* Step Number */}
-        <p className="text-basegreen text-lg sm:text-xl md:text-2xl font-semibold">
+        <p className="text-basegreen text-lg sm:text-xl md:text-xl font-semibold">
           {number}
         </p>
 
         {/* Card Title */}
-        <p className="text-white text-xl sm:text-2xl md:text-3xl font-bold leading-tight">
+        <p className="text-white text-xl sm:text-2xl md:text-2xl leading-tight">
           {title}
         </p>
 
